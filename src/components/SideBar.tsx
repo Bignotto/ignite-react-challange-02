4e7ff5f7af9ce4eef55dc50db2bc715a6ae0027d
Thiago Bignotto <bignotto@gmail.com>
2021-04-17 16:32:39 -0300

    Challange concluded

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
+import { useState } from "react";
+import { Button } from "./Button";
+import "../styles/sidebar.scss";
+
+interface MovieGenre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface MoviesGenresProps {
+  genres: MovieGenre[];
+}
+
+export function SideBar(props: MoviesGenresProps) {
   // Complete aqui
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
